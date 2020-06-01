e34aa8deb7a39a2d565f9e8bd141235e3f021374
Artyom Sovetnikov <elringus@yandex.ru>
2020-06-01 20:10:33 +0300

    add remove all items command

---
+++ Assets/NaninovelInventory/Runtime/Commands/RemoveAllItems.cs
@@ -0,0 +1,23 @@
+﻿using Naninovel;
+using Naninovel.Commands;
+using System.Threading;
+using UniRx.Async;
+
+namespace NaninovelInventory
+{
+    /// <summary>
+    /// Removes all item in the inventory.
+    /// </summary>
+    public class RemoveAllItems : Command
+    {
+        public override UniTask ExecuteAsync (CancellationToken cancellationToken = default)
+        {
+            var uiManager = Engine.GetService<IUIManager>();
+            var inventory = uiManager.GetUI<InventoryUI>();
+
+            inventory.RemoveAllItems();
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
